e82559db1f4e93bdbd61f36d98750d78a1cba781
Junichi Ito <jit@sonicgarden.jp>
2018-04-26 09:58:11 +0900

    Refactoring (split run method)

--- lib/app.rb
+++ lib/app.rb
@@ -9,59 +9,9 @@ class App
 
   def run
     init_app
-
-    puts "ブラックジャックへようこそ！"
-    puts "ゲームを開始します。"
-
-    @player.cards.each do |card|
-      show_player_card(card)
-    end
-
-    show_dealer_card(@dealer.cards[0])
-    puts "ディーラーの2枚目のカードは分かりません。"
-
-    begin
-      puts "あなたの現在の得点は#{@player.total}です。"
-      puts "カードを引きますか？"
-    end while player_hit? && !@player.bust?
-
-    if @player.bust?
-      puts "#{@player.total}点でバストしました。"
-      result = :lose
-    else
-      puts "ディーラーの2枚目のカードは#{@dealer.cards[1]}でした。"
-
-      begin
-        puts "ディーラーの現在の得点は#{@dealer.total}です。"
-      end while dealer_hit?
-
-      if @dealer.bust?
-        puts "#{@dealer.total}点でバストしました。"
-        result = :win
-      else
-        puts "あなたの得点は#{@player.total}です。"
-        puts "ディーラーの得点は#{@dealer.total}です。"
-
-        if @player.total == @dealer.total
-          result = :draw
-        elsif @player.total > @dealer.total
-          result = :win
-        else
-          result = :lose
-        end
-      end
-    end
-
-    result_text =
-      case result
-      when :win then "あなたの勝ちです！"
-      when :lose then "あなたの負けです。"
-      when :draw then "引き分けです。"
-      else raise "Unknown result: #{result}"
-      end
-
-    puts result_text
-    puts "ブラックジャック終了！また遊んでね★"
+    show_intro
+    result = play_game
+    show_outro(result)
   end
 
   private
@@ -72,12 +22,55 @@ class App
     @dealer = Dealer.new(*@cards.shift(2))
   end
 
-  def show_player_card(card)
-    puts "あなたの引いたカードは#{card}です。"
+  def show_intro
+    puts "ブラックジャックへようこそ！"
+    puts "ゲームを開始します。"
+
+    @player.cards.each do |card|
+      show_player_card(card)
+    end
+
+    show_dealer_card(@dealer.cards[0])
+    puts "ディーラーの2枚目のカードは分かりません。"
   end
 
-  def show_dealer_card(card)
-    puts "ディーラーの引いたカードは#{card}です。"
+  def play_game
+    begin
+      puts "あなたの現在の得点は#{@player.total}です。"
+      puts "カードを引きますか？"
+    end while player_hit? && !@player.bust?
+
+    if @player.bust?
+      puts "#{@player.total}点でバストしました。"
+      :lose
+    else
+      puts "ディーラーの2枚目のカードは#{@dealer.cards[1]}でした。"
+
+      begin
+        puts "ディーラーの現在の得点は#{@dealer.total}です。"
+      end while dealer_hit?
+
+      if @dealer.bust?
+        puts "#{@dealer.total}点でバストしました。"
+        :win
+      else
+        puts "あなたの得点は#{@player.total}です。"
+        puts "ディーラーの得点は#{@dealer.total}です。"
+
+        if @player.total == @dealer.total
+          :draw
+        elsif @player.total > @dealer.total
+          :win
+        else
+          :lose
+        end
+      end
+    end
+  end
+
+  def show_outro(result)
+    puts result_text(result)
+    puts "ブラックジャック終了！また遊んでね★"
   end
 
   def generate_cards
@@ -100,6 +93,23 @@ class App
       show_dealer_card(card)
       @dealer.hit(card)
       true
+    end
+  end
+
+  def show_player_card(card)
+    puts "あなたの引いたカードは#{card}です。"
+  end
+
+  def show_dealer_card(card)
+    puts "ディーラーの引いたカードは#{card}です。"
+  end
+
+  def result_text(result)
+    case result
+    when :win then "あなたの勝ちです！"
+    when :lose then "あなたの負けです。"
+    when :draw then "引き分けです。"
+    else raise "Unknown result: #{result}"
     end
   end
 end
