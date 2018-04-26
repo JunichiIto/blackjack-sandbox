b1200a15ae981ca78bbca671b4efa12c9c97516f
Junichi Ito <jit@sonicgarden.jp>
2018-04-26 12:58:39 +0900

    Add spec for game repeat

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -261,5 +261,131 @@ describe App do
         expect { app.run }.to output(expected).to_stdout
       end
     end
+
+    context 'when repeat game' do
+      let(:cards) do
+        [
+          Card.new(:heart, 'K'),
+          Card.new(:diamond, 'A'),
+          Card.new(:diamond, '5'),
+          Card.new(:heart, '3'),
+          Card.new(:heart, 'A'),
+          Card.new(:diamond, '10'),
+
+          Card.new(:diamond, 'A'),
+          Card.new(:diamond, '10'),
+          Card.new(:spade, '8'),
+          Card.new(:diamond, '2'),
+          Card.new(:club, 'K'),
+          Card.new(:diamond, '9'),
+
+          Card.new(:spade, 'A'),
+          Card.new(:spade, '10'),
+          Card.new(:club, '3'),
+          Card.new(:diamond, '10'),
+          Card.new(:diamond, '4'),
+        ]
+      end
+      let(:expected) do
+        <<~TEXT
+          ブラックジャックへようこそ！
+
+          【第1回戦】
+          ゲームを開始します。
+
+          あなたの引いたカードはハートのKです。
+          あなたの引いたカードはダイヤのAです。
+
+          ディーラーの引いたカードはダイヤの5です。
+          ディーラーの2枚目のカードは分かりません。
+
+          あなたの現在の得点は11です。
+          カードを引きますか？
+          y/n: 
+          あなたの引いたカードはハートのAです。
+          あなたの現在の得点は12です。
+          カードを引きますか？
+          y/n: 
+          あなたの引いたカードはダイヤの10です。
+          22点でバストしました。
+          あなたの負けです。
+
+          対戦成績: 0勝1敗0分
+          勝率: 0.0%
+
+          ブラックジャック終了！もう一度遊びますか？
+          y/n: 
+          【第2回戦】
+          ゲームを開始します。
+
+          あなたの引いたカードはダイヤのAです。
+          あなたの引いたカードはダイヤの10です。
+
+          ディーラーの引いたカードはスペードの8です。
+          ディーラーの2枚目のカードは分かりません。
+
+          あなたの現在の得点は11です。
+          カードを引きますか？
+          y/n: 
+          あなたの引いたカードはクラブのKです。
+          あなたの現在の得点は21です。
+          カードを引きますか？
+          y/n: 
+          ディーラーの2枚目のカードはダイヤの2でした。
+          ディーラーの現在の得点は10です。
+          press return: 
+          ディーラーの引いたカードはダイヤの9です。
+          ディーラーの現在の得点は19です。
+          press return: 
+          あなたの得点は21です。
+          ディーラーの得点は19です。
+          あなたの勝ちです！
+
+          対戦成績: 1勝1敗0分
+          勝率: 50.0%
+
+          ブラックジャック終了！もう一度遊びますか？
+          y/n: 
+          【第3回戦】
+          ゲームを開始します。
+
+          あなたの引いたカードはスペードのAです。
+          あなたの引いたカードはスペードの10です。
+
+          ディーラーの引いたカードはクラブの3です。
+          ディーラーの2枚目のカードは分かりません。
+
+          あなたの現在の得点は11です。
+          カードを引きますか？
+          y/n: 
+          ディーラーの2枚目のカードはダイヤの10でした。
+          ディーラーの現在の得点は13です。
+          press return: 
+          ディーラーの引いたカードはダイヤの4です。
+          ディーラーの現在の得点は17です。
+          press return: 
+          あなたの得点は11です。
+          ディーラーの得点は17です。
+          あなたの負けです。
+
+          対戦成績: 1勝2敗0分
+          勝率: 33.3%
+
+          ブラックジャック終了！もう一度遊びますか？
+          y/n: 
+          さようなら。また遊んでね★
+        TEXT
+      end
+      example do
+        app = App.new
+        allow(app).to receive(:generate_cards).and_return(cards)
+        allow(STDIN).to receive(:gets).and_return(
+          'y', 'y', 'y',
+          'y', 'n', '', '', 'y',
+          'n', '', '', 'n',
+        )
+        expect { app.run }.to output(expected).to_stdout
+      end
+    end
   end
 end
