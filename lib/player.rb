9256457632231e10484e238da3aba1700002bf75
Junichi Ito <jit@sonicgarden.jp>
2018-04-27 12:44:22 +0900

    Remove unused property

--- lib/player.rb
+++ lib/player.rb
@@ -3,7 +3,7 @@ require './lib/playable'
 class Player
   include Playable
 
-  attr_reader :cards, :stand
+  attr_reader :cards
 
   def initialize(first_card, second_card)
     @cards = [first_card, second_card]
